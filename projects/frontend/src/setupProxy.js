686ddf7bf8e878d815689feca7c359ac47b20d4e
PineAG <pein.agail@gmail.com>
2022-03-13 22:52:55 +0800

    init frontend

---
+++ projects/frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+const {createProxyMiddleware} = require('http-proxy-middleware');
+module.exports = function (app) {
+    app.use(
+        '/data',
+        createProxyMiddleware({
+            target : 'http://127.0.0.1:3001/',
+            changeOrigin : true,
+            pathRewrite : {
+                '^/data' : ''
+            }
+        })
+    );
+};
